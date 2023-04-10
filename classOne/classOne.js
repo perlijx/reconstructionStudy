b8639e1b03c91d0dbe5bbacaa99a3909bd799705
perli <1003914407@qq.com>
2023-04-10 17:29:31 +0800

    feat: 将一个函数的返回值从赋值给临时变量变为替换成一个明确声明的函数

--- classOne/classOne.js
+++ classOne/classOne.js
@@ -2,9 +2,6 @@ module.exports = function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency", currency: "USD", minimumFractionDigits: 2
-  }).format;
   for (let perf of invoice.performances) {
     // 添加数量积分
     volumeCredits += volumeCreditsFor(perf);
@@ -48,6 +45,14 @@ function amountFor(perf) {
   return result
 }
 
+function format(aNumber) {
+  return new Intl.NumberFormat("en-US",
+    {
+      style: "currency", currency: "USD",
+      minimumFractionDigits: 2
+    }).format(aNumber);
+}
+  
 
 function volumeCreditsFor(aPerformance) {
   let result = 0;
